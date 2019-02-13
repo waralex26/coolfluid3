dd1b234b1aad982417c08223741a666d21ed6095
waralex26 <alexandre@localhost.localdomain>
2019-02-13 10:09:37 +0100

    Test  on AdjointCt.cpp

--- plugins/UFEM/src/UFEM/adjoint/AdjointCt.cpp
+++ plugins/UFEM/src/UFEM/adjoint/AdjointCt.cpp
@@ -176,8 +176,8 @@ void AdjointCt::trigger_assembly()
                   _T(U[_i], U[_i]) += transpose(N(U) - tau_su*u*nabla(U)) * N(U), // Time, standard and SUPG
                   _a[U[_i]] += -transpose(N(U) - tau_su*u*nabla(U)) * 3 * g[_i] * density_ratio + 
                             m_turbulence*(-(transpose(N(U) - tau_su*u*nabla(U))*ka*gradient(k)[_i]) - (transpose(N(U) - tau_su*u*nabla(U))*epsilona*gradient(epsilon)[_i])
-                                            +(2*((ka*k/epsilon)+(epsilona*m_c_epsilon_1))*k*m_c_mu* transpose(nabla(U)) *_col(partial(u[_i],_j)+partial(u[_j],_i),_i))),
-                  _A(U[_i], U[_i]) += transpose(N(U) - tau_su*nabla(U)) * N(U) * normal[_i] * Ct * uDisk / lit(m_th)
+                                            +(2*((ka*k/epsilon)+(epsilona*m_c_epsilon_1))*k*m_c_mu* transpose(nabla(U)) *_col(partial(u[_i],_j)+partial(u[_j],_i),_i)))
+                  //_A(U[_i], U[_i]) += transpose(N(U) - tau_su*nabla(U)) * N(U) * Ct * uDisk / lit(m_th)
           ),
         system_rhs += -_A * _x + _a,
         _A(q) = _A(q) / theta,
@@ -196,7 +196,7 @@ void AdjointCt::trigger_assembly()
 
                             element_quadrature
                             (
-                                // _A(U[_i], U[_i]) += transpose(N(U)) * N(U) * lit(m_ct[Nt]) * u[0] / lit(m_th) * normal[_i] ,
+                                _A(U[_i], U[_i]) += transpose(N(U)) * N(U) * lit(m_ct[Nt]) * u[0] / lit(m_th) * normal[_i] ,
                                 // _a[U[_i]] += transpose(N(U)) * lit(3.0) / lit(2.0) * lit(m_ct[Nt]) * u[0] * u[0] * normal[_i]
                             ), // integrate
                             system_rhs +=-_A * _x + _a, // update global system RHS with element vector
