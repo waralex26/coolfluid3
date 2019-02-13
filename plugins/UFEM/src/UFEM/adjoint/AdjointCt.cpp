4682039f190674d2f6bd98521a1c251c07ecf100
waralex26 <alexandre@localhost.localdomain>
2019-02-13 20:31:17 +0100

    Test on AdjointCt.cpp

--- plugins/UFEM/src/UFEM/adjoint/AdjointCt.cpp
+++ plugins/UFEM/src/UFEM/adjoint/AdjointCt.cpp
@@ -196,7 +196,7 @@ void AdjointCt::trigger_assembly()
 
                             element_quadrature
                             (
-                                _A(U[_i], U[_i]) += transpose(N(U)) * N(U) * Ct * uDisk /* / lit(m_th) */ * normal[_i],
+                                _A(U[_i], U[_i]) += transpose(N(U)) * N(U) * Ct * u[0] /* / lit(m_th) */ * normal[_i],
                                 _a[U[_i]] += transpose(N(U)) * lit(3.0) / lit(2.0) * lit(m_ct[Nt]) * u[0] * u[0] * normal[_i]
                             ), // integrate
                             system_rhs +=-_A * _x + _a, // update global system RHS with element vector
