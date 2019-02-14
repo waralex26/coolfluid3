4946e5dade0811b684ff25d37fbf8a7b0de934bb
waralex26 <alexandre@localhost.localdomain>
2019-02-14 08:15:30 +0100

    Test on ActuatorDiskCt.cpp

--- plugins/UFEM/src/UFEM/adjoint/ActuatorDiskCt.hpp
+++ plugins/UFEM/src/UFEM/adjoint/ActuatorDiskCt.hpp
@@ -50,7 +50,7 @@ private:
   Real m_u_mean_disk = 0.;
 
   FieldVariable<0, ScalarField> Ct;
-  FieldVariable<1, ScalarField> uDisk;
+  FieldVariable<1, VectorField> uDisk;
 
   cf3::solver::actions::Proto::SystemRHS rhs;
   cf3::solver::actions::Proto::SystemMatrix system_matrix;
