7ad4cad51e343bdbd7dfcd90e3ec0e6b9c001435
waralex26 <alexandre@localhost.localdomain>
2019-02-21 16:01:34 +0100

    Test on AdjointCt.cpp

--- plugins/UFEM/src/UFEM/adjoint/AdjointCt.cpp
+++ plugins/UFEM/src/UFEM/adjoint/AdjointCt.cpp
@@ -174,8 +174,8 @@ void AdjointCt::trigger_assembly()
                                       //+ 0.5*u[_i]*(N(U) - tau_su*u*nabla(U)) * nabla(U)[_j], //  skew symmetric part of advection (standard +SUPG)
                   _T(q    , U[_i]) += tau_ps * transpose(nabla(q)[_i]) * N(U), // Time, PSPG
                   _T(U[_i], U[_i]) += transpose(N(U) - tau_su*u*nabla(U)) * N(U), // Time, standard and SUPG
-                  _a[U[_i]] += transpose(N(U) -tau_su*u*nabla(U)) * lit(3.0) / lit(2.0) * Ct * uDisk[_i] * uDisk[_i] / lit(m_th) * density_ratio 
-                          // -transpose(N(U) - tau_su*u*nabla(U)) * lit(3.0) * g[_i] * density_ratio + 
+                  _a[U[_i]] += // transpose(N(U) -tau_su*u*nabla(U)) * lit(3.0) / lit(2.0) * Ct * uDisk[_i] * uDisk[_i] / lit(m_th) * density_ratio 
+                          -transpose(N(U) - tau_su*u*nabla(U)) * lit(3.0) * g[_i] * density_ratio + 
                            - transpose(N(U) -tau_su*u*nabla(U)) * lit(m_U_mean_disk) * Ct * uDisk[0] / lit(m_th) * density_ratio 
                             // transpose(N(U) -tau_su*u*nabla(U)) * lit(3.0) * g[_i] * density_ratio 
                             // -transpose(N(U) -tau_su*u*nabla(U)) * lit(2.0) * lit(m_U_mean_disk) * g[_i] / lit(12.9955) * density_ratio
