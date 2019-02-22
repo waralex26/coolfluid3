d0235c7f91616eec028648e50784ca5198bd4e21
waralex26 <alexandre@localhost.localdomain>
2019-02-22 21:49:47 +0100

    Test on AdjointCt.cpp

--- plugins/UFEM/src/UFEM/adjoint/AdjointCt.cpp
+++ plugins/UFEM/src/UFEM/adjoint/AdjointCt.cpp
@@ -186,8 +186,8 @@ void AdjointCt::trigger_assembly()
           ),
         system_rhs += -_A * _x + _a,
         _A(q) = _A(q) / theta, 
-        system_matrix += invdt() * _T + theta * _A,
-        //_cout << transpose(_a) << "\n"
+        system_matrix += invdt() * _T + theta * _A
+        //, _cout << transpose(_a) << "\n"
       )
     )
   ));
