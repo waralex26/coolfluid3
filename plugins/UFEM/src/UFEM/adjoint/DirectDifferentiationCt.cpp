13a95b0fa3630f631f7a369e536d2e7033e70346
waralex26 <alexandre@localhost.localdomain>
2019-02-22 20:29:32 +0100

    Test on DirectDifferentiationCt.cpp

--- plugins/UFEM/src/UFEM/adjoint/DirectDifferentiationCt.cpp
+++ plugins/UFEM/src/UFEM/adjoint/DirectDifferentiationCt.cpp
@@ -189,7 +189,8 @@ void DirectDifferentiationCt::trigger_assembly()
         //compute_tau.apply(u, nu_eff, lit(dt()), lit(tau_ps), lit(tau_su), lit(tau_bulk)),
         element_quadrature
         (
-          _a[SensU[_i]] += - transpose(N(SensU)) * normal[_i] * lit(0.5) * uDisk[_i] * uDisk[_i] /* / lit(m_th) */ * density_ratio
+          // _a[SensU[_i]] += - transpose(N(SensU)) * normal[_i] * lit(0.5) * uDisk[_i] * uDisk[_i] /* / lit(m_th) */ * density_ratio
+          _a[SensU[_i]] += -transpose(N(SensU)) * normal[_i] * g[_i] / lit(m_ct[Nt])
         ),
         // element_quadrature(_A(SensU[_i], SensU[_i]) += transpose(N(SensU))*N(SensU)*u[_i]* lit(4) * lit(m_a[Nt])/(lit(1)-lit(m_a[Nt]))/ lit(m_th)*density_ratio * normal[_i]), // integrate
         system_rhs += _a
