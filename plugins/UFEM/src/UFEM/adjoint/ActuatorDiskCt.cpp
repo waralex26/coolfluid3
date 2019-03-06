aa6b4de062cc31973cd13cbe51163c3682381f1a
waralex26 <alexandre@localhost.localdomain>
2019-03-06 09:02:26 +0100

    Test on ActuatorDiskCt.cpp

--- plugins/UFEM/src/UFEM/adjoint/ActuatorDiskCt.cpp
+++ plugins/UFEM/src/UFEM/adjoint/ActuatorDiskCt.cpp
@@ -158,7 +158,7 @@ void ActuatorDiskCt::execute()
 
   // surface_integral(m_u_mean_disk, std::vector<Handle<mesh::Region>>({m_loop_regions[1]}), _abs((u*normal)[0]));
   // m_u_mean_disk /= m_area;
-  volume_integral(m_u_mean_disk, std::vector<Handle<mesh::Region>>({m_loop_regions[0]}), (u*normal)[0]);
+  volume_integral(m_u_mean_disk, std::vector<Handle<mesh::Region>>({m_loop_regions[0]}), u[0]);
   // volume_integral(m_u_mean_disk, std::vector<Handle<mesh::Region>>({m_loop_regions[0]}), lit(1.0));
   m_u_mean_disk /= (m_area * m_th);
 
